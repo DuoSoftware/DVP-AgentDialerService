fed9a9b10e4a1bddb77625caeaa7b6efc73d587b
sukitha <sukitha@duosoftware.com>
2017-06-30 17:33:31 +0530

    ioredis added with sentinel support

--- RedisHandler.js
+++ RedisHandler.js
@@ -7,18 +7,90 @@
 
 /*var format = require("stringformat");*/
 var config = require("config");
-var redis = require("redis");
-var redisip = config.Redis.ip;
-var redisport = config.Redis.port;
-var redisClient = redis.createClient(redisport, redisip);
+var redis = require("ioredis");
 var messageFormatter = require("dvp-common/CommonMessageGenerator/ClientMessageJsonFormatter.js");
 /*var logger = require("dvp-common/LogHandler/CommonLogHandler.js").logger;*/
 
-redisClient.auth(config.Redis.password, function (err) {
-    /*if (err)
-     throw err;*/
-    console.error("Redis Auth error ", err);
-});
+var redisip = config.Redis.ip;
+var redisport = config.Redis.port;
+var redispass = config.Redis.password;
+var redismode = config.Redis.mode;
+var redisdb = config.Redis.db;
+
+
+
+var redisSetting =  {
+    port:redisport,
+    host:redisip,
+    family: 4,
+    password: redispass,
+    db: redisdb,
+    retryStrategy: function (times) {
+        var delay = Math.min(times * 50, 2000);
+        return delay;
+    },
+    reconnectOnError: function (err) {
+
+        return true;
+    }
+};
+
+if(redismode == 'sentinel'){
+
+    if(config.Redis.sentinels && config.Redis.sentinels.hosts && config.Redis.sentinels.port, config.Redis.sentinels.name){
+        var sentinelHosts = config.Redis.sentinels.hosts.split(',');
+        if(Array.isArray(sentinelHosts) && sentinelHosts.length > 2){
+            var sentinelConnections = [];
+
+            sentinelHosts.forEach(function(item){
+
+                sentinelConnections.push({host: item, port:config.Redis.sentinels.port})
+
+            })
+
+            redisSetting = {
+                sentinels:sentinelConnections,
+                name: config.Redis.sentinels.name,
+                password: redispass
+            }
+
+        }else{
+
+            console.log("No enough sentinel servers found .........");
+        }
+
+    }
+}
+
+var redisClient = undefined;
+
+if(redismode != "cluster") {
+    redisClient = new redis(redisSetting);
+}else{
+
+    var redisHosts = redisip.split(",");
+    if(Array.isArray(redisHosts)){
+
+
+        redisSetting = [];
+        redisHosts.forEach(function(item){
+            redisSetting.push({
+                host: item,
+                port: redisport,
+                family: 4,
+                password: redispass});
+        });
+
+        var redisClient = new redis.Cluster([redisSetting]);
+
+    }else{
+
+        redisClient = new redis(redisSetting);
+    }
+
+
+}
+
 
 redisClient.on("error", function (err) {
     console.error("Redis connection error ", err);
@@ -28,7 +100,7 @@ redisClient.on("connect", function (err) {
     if(err){
         console.error(err);
     }
-    redisClient.select(config.Redis.redisdb, redis.print);
+
 });
 
 module.exports.collectJobList = function (company, iss, jobId) {
